e1809598a66d5dbbced653c4c4c7b693ebfe229a
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-05-02 16:05:17 +0200

    feat added vue projects api

---
+++ boolfolio/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Projects from './components/Projects.vue'
+export default {
+  components: {
+    Projects
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">
+      Projects
+    </h1>
+    <Projects />
+  </div>
+</template>
+
+<style >
+.title {
+  padding: 2rem 0;
+}
+</style>
